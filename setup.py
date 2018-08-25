b8775f3cba0939cd56a5f398423a528cbd0dd0f0
philipperemy <premy@cogent.co.jp>
2018-08-25 10:22:04 +0900

    Bitcoin data in the generic case

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from Cython.Build import cythonize
 from distutils.core import setup
 
-setup(name='lead_lag', ext_modules=cythonize("lead_lag.pyx"))
+setup(name='lead_lag',
+      ext_modules=cythonize("lead_lag.pyx"))
