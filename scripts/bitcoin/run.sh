9cfb73996751c72407b58550f8b1f1639f8d1d5b
philipperemy <premy@cogent.co.jp>
2018-08-26 19:10:26 +0900

    Refactoring

--- scripts/bitcoin/run.sh
+++ scripts/bitcoin/run.sh
@@ -5,3 +5,5 @@ python convert_bitcoinchart_file.py ~/Dropbox/lead-lag/raw-data/wexUSD.csv /tmp/
 
 python split_dataset_per_day.py /tmp/bitstampUSD.csv.processed /tmp/bitcoin
 python split_dataset_per_day.py /tmp/wexUSD.csv.processed /tmp/bitcoin
+
+python main.py /tmp/bitcoin /tmp/bitcoin_contrasts/
