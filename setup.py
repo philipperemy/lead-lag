e540c2d432cd6e641abf2927ad971df99ce960d7
philipperemy <premy.enseirb@gmail.com>
2022-09-28 23:00:27 +0700

    compatibility break -> v2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 
 from setuptools import setup, find_packages
 
-VERSION = '1.7'
+VERSION = '2.0'
 
 # Cython has to be installed before. And I could not find any other ways.
 os.system('pip install cython')
@@ -14,17 +14,19 @@ from Cython.Build import cythonize  # noqa: E402
 if sys.version_info[0] < 3:
     raise Exception('Must be using Python 3.')
 
-setup(name='lead-lag',
-      version=VERSION,
-      ext_modules=cythonize("lead_lag/lead_lag_impl.pyx", language_level="3"),
-      description='Lead lag estimation with a O(n log n) complexity.',
-      author='Philippe Remy',
-      license='Open Source',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=[
-          'pandas>=0.22.0',
-          'numpy>=1.15.0',
-          'tqdm>=4.19.2',
-          'matplotlib>=2.2.2',
-      ])
+setup(
+    name='lead-lag',
+    version=VERSION,
+    ext_modules=cythonize("lead_lag/lead_lag_impl.pyx", language_level="3"),
+    description='Lead lag estimation with a O(n log n) complexity.',
+    author='Philippe Remy',
+    license='Open Source',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pandas>=0.22.0',
+        'numpy>=1.15.0',
+        'tqdm>=4.19.2',
+        'matplotlib>=2.2.2',
+    ]
+)
